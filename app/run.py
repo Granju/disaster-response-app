cfbb7f3cecd24afddf5881d48a1d504941949488
Julien Flandre <julien.flandre@gmail.com>
2022-06-01 08:50:55 +0100

    tokenize called from separate script in run.py

--- app/run.py
+++ app/run.py
@@ -14,35 +14,11 @@ from sqlalchemy import create_engine
 from sklearn.base import BaseEstimator, TransformerMixin
 import re
 
+from utilities.tokenize import tokenize
+
 
 
 app = Flask(__name__)
-
-def tokenize(text):
-    """
-    Create lemmatized tokens from words in a string.
-
-    Input:
-    A string made of oen to several sentences.
-
-    Output:
-    A list of tokenised and lemmatized words.
-    """
-    # Replacing urls in text with placeholder
-    detected_urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
-    for url in detected_urls:
-        text = text.replace(url,"urlplaceholder")
-
-
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
 
 class VerbCounter(BaseEstimator, TransformerMixin):
     """
